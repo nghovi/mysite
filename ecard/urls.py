c04dac4a428e6b32d7ba873d5365564c61189c02
nguyen.viet <vietnh90@gmail.com>
2015-10-04 15:24:07 +0700

    Fix bug

--- ecard/urls.py
+++ ecard/urls.py
@@ -5,9 +5,13 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
 
-    url(r'^get_tasks/$', views.task_list, name='get_tasks'),
+    url(r'^get_primary_card_info/$', views.get_books, name='get_primary_card_info'),
+
+
+    url(r'^get_tasks/$', views.get_tasks, name='get_tasks'),
     url(r'^delete_task/(?P<task_id>[0-9]+)/$', views.delete_task, name='delete_task'),
+    url(r'^add_task/$', views.add_task, name='add_task'),
+    url(r'^update_status_task/$', views.update_status_task, name='update_status_task'),
 
-
-    url(r'^get_books/$', views.book_list, name='get_books'),
+    url(r'^get_books/$', views.get_books, name='get_books'),
 ]
--- ecard/views.py
+++ ecard/views.py
@@ -16,20 +16,29 @@ from .models import Link
 def index(request):
     return HttpResponse("Hello, world. You're at the ecard index.")
 
-def task_list(request):
-    response2 = ""
-    for key,value in request.META.items():
-        response2 += key
-        if type(value) is str:
-            response2 = response2 + ":" + value
+def get_primary_card_info(request):
+    return HttpResponse("todo")
 
-    latest_task_list = Task.objects.order_by('-date')[:5]
-    # output = ', '.join([task.name for task in latest_task_list])
+def get_books(request):
+    response = "You're requesting book list %s."
+    return HttpResponse(response)
+
+
+########################### Task######################################
+def get_tasks(request):
+    # response2 = ""
+    # for key,value in request.META.items():
+    #     response2 += key
+    #     if type(value) is str:
+    #         response2 = response2 + ":" + value
+        # output = ', '.join([task.name for task in latest_task_list])
     # return HttpResponse("You're requesting task list: <br/>" + output)
     # In order to serialize objects other than dict you must set the safe parameter to False:
+    # return HttpResponse(response2)
+
+    latest_task_list = Task.objects.order_by('-date')[:5]
     response = serializers.serialize("json", latest_task_list)
-    return HttpResponse(response2)
-    #return JsonResponse(json.loads(response) + response2, safe=False)
+    return JsonResponse(json.loads(response), safe=False)
 
 def add_task(request):
     return HttpResponse("sfsf")
@@ -37,6 +46,6 @@ def add_task(request):
 def delete_task(request, task_id):
     return HttpResponse("You're going to delete task id " + task_id)
 
-def book_list(request):
-    response = "You're requesting book list %s."
-    return HttpResponse(response)
+def update_status_task(request, task_id, status):
+    return HttpResponse("todo")
+
